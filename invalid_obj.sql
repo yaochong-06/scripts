895d73ab3b83cc2bf4f0c9bb84b1cc5df2c45196
chongzi <15201061106@163.com>
2021-06-19 23:17:20 +0800

    de

--- invalid_obj.sql
+++
@@ -1,4 +0,0 @@
-col object_name format a35
-select owner,object_type,object_name,status,created from dba_objects
-where status <> 'VALID' 
-order by owner,object_type,object_name;
--- registry.sql
+++
@@ -1,8 +0,0 @@
-col action format a6
-col namespace format a10
-col version format a10
-col comments format a15
-col action_time format a30
-col bundle_series format a15
-select * from dba_registry_history;
-
