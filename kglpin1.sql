7bb45ab04aaf69b6d329cfb30193fa0987890990
chongzi <15201061106@163.com>
2021-07-08 22:39:31 +0800

    add

--- kglpin1.sql
+++
@@ -1,4 +0,0 @@
--- usage:   @kglpin1 <objname>
--- example: @kglpin DUMMY_PROC
-
-select kglhdadr, kglnaown, kglnaobj from x$kglob where kglnaobj = '&1';
--- kglpin2.sql
+++
@@ -1,4 +0,0 @@
--- usage:   @kglpin2 <addr>
--- example: @kglpin2 177B52878
-
-select kglhdadr, kglnaown, kglnaobj from x$kglob where kglhdadr = hextoraw(lpad('&1', vsize(w.kgllkhdl)*2, 0));
